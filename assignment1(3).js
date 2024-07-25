825dd3d0813586b40bac972fb76773e43dfa1e3d
niharika7529 <168266376+niharika7529@users.noreply.github.com>
2024-07-25 19:30:10 +0530

    Add files via upload

---
+++ assignment1(3).js
@@ -0,0 +1,9 @@
+//write a program to check number is divisible
+ n=prompt("enter a number")
+ if(n%3==0)
+    {
+     console.log("the given number is divisible by 3")
+ }
+ else{
+    console.log("the given number is not divisible by 3")
+ }
